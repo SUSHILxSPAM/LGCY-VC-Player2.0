e0c6d9d209195c5065a938980266203bc7145200
SUSHILxSPAM <100059120+SUSHILxSPAM@users.noreply.github.com>
2022-02-23 21:22:31 +0530

    Update player.py

--- plugins/player.py
+++ plugins/player.py
@@ -89,7 +89,7 @@ async def generate_cover(requested_by, title, views, duration, thumbnail):
     draw.text((190, 630), f"Views: {views}", (255, 255, 255), font=font)
     draw.text(
         (190, 670),
-        f"Powered By:- 𝙍𝙘𝙍 𝙊𝙒𝙉𝙀𝙍 (@NAVYA_KESHRI)",
+        f"Powered By:- RcR OWNER (@NAVYA_KESHRI)",
         (255, 255, 255),
         font=font,
     )
